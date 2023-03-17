3989aba92e0e085da13a8953b48087cfe27de527
drew <shanetydrew-byte@proton.me>
2023-03-17 22:32:49 +0800

    init

---
+++ index.js
@@ -0,0 +1,15 @@
+for (var i = 0 ; i < document.querySelectorAll(".drum").length ; i++) {
+
+    document.querySelectorAll(".drum")[i].addEventListener("click",function ()
+    {
+        var clicked = this.innerHTML;
+
+        
+    })
+   
+}
+
+function keyDownSound()
+{
+    
+}
